0c58d2d3998253e17dd211accce39c9b2619596b
Jeff Ward <jeffwardcoding@gmail.com>
2015-07-08 12:20:49 -0400

    fixed reload button
    Need to fix layout to horizontal

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ var red;
 var green;
 var blue;
 var rgbColor;
-var log = logger('output');
 
 function randomRGB() {
   return Math.floor(Math.random() * 256 );
@@ -18,49 +17,20 @@ function randomColor() {
 }
 
 function print(message) {
-<<<<<<< HEAD
  document.getElementById('output').innerHTML = (message);
 }
 
 function build() {
-=======
- log(message);
-}
-
-function generate(){
->>>>>>> origin/master
 for (var i = 0; i < 10; i += 1) {
   rgbColor = randomColor();
-  html += '<div style="background-color:' + rgbColor + ' display:inline" ></div>';
+  html += '<div style="background-color:' + rgbColor + '"></div>';
 }
-}
-
 }
   
-
 function reload() {
-<<<<<<< HEAD
   html = '';
   build();
-=======
-  generate();
->>>>>>> origin/master
   print(html);
 }
 build();
 print(html);
-
-function logger(x) {
-var output = getById(x);
-  return function (text) {
-    output.innerHTML += text + '\n';
-  };
-
-}
-
-function getById(x) {
- return document.getElementById(x);
-}
-////Github:
-//$ cat .gitattributes
-//  *.rb linguist-language=JavaScript
